dfb98a80bdbdded8e6bd27f025bbe6b1c977c90c
Steven <ssebexen@gmail.com>
2024-08-20 11:45:43 -0400

    Made character portrait generation dynamic.

---
+++ scenes/CharacterSelect/CharacterSelect.gd
@@ -0,0 +1,26 @@
+extends Control
+
+var CHARACTER_NAMES: Array[String] = [
+	"Brian", "Bruno", "Dorothy", "Iyana", "Jacob",
+	"Josh", "Lui", "Mehul", "Norman", "Parth",
+	"Pranav", "Sam", "Sarah", "Steven"
+]
+
+var CharacterCard = load("res://scenes/CharacterSelect/CharacterCard.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	create_character_card_box()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func create_character_card_box():
+	for i in range(0, CHARACTER_NAMES.size() - 1):
+		var character_card = CharacterCard.instantiate()
+		character_card.portrait = load("res://assets/portraits/%s.png" % CHARACTER_NAMES[i].to_lower())
+		character_card.character_name = CHARACTER_NAMES[i]
+		$CharacterCards.add_child(character_card)
